3a159d454460700b41d0e23ced6eda49a929f5b3
Ivan Lugo <ivan.lugo@avast.com>
2021-01-29 18:29:57 -0800

    - Fix imports

--- src/main/kotlin/visual_interfaces/web/htmlPages/AboutPage.kt
+++ src/main/kotlin/visual_interfaces/web/htmlPages/AboutPage.kt
@@ -5,7 +5,7 @@ import visual_interfaces.web.htmlComponents.SimpleHTML.div
 import visual_interfaces.web.htmlComponents.SimpleHTML.localImage
 import visual_interfaces.web.htmlComponents.SimpleHTML.setCssClasses
 import visual_interfaces.web.htmlComponents.SimpleHTML.text
-import visual_interfaces.web.htmlComponents.ComponentClasses.MainPage.devImage
+import visual_interfaces.web.htmlComponents.ComponentClasses.AboutPage.devImage
 import visual_interfaces.web.htmlComponents.ComponentClasses.MainPage.mainBodyWrapper
 
 fun SimpleHTML.Html.makeAboutPageContent() {
